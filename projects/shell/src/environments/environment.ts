3919b30bff0b56b0ec83e257550142755598e7ba
Andrey <amildream@gmail.com>
2019-08-06 16:33:46 +0700

    fix environment

--- projects/shell/src/environments/environment.ts
+++ projects/shell/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
     production: false,
     api: 'https://api.paymentwall.com/api/payment-systems',
-    geolocationApi: 'http://ipinfo.io',
+    geolocationApi: 'https://ipinfo.io',
     geolocationToken: 'eaf3441bebf7c2',
     countriesApi: 'https://restcountries.eu',
     projectKey: '221b9fc0ef6fa22503e594565a16ae64'
